ba356067506082f4c4fca13ceaae6608a826dc9e
Daniil7 <den.urishov@yandex.ru>
2022-10-11 17:23:33 +0400

    programm v1

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // которая из имеющегося массива строк 
 // формирует массив из строк длина которых 
 // меньше либо равна 3 символа.
-string[] array = {"football", "soccer", "123", "12"};
+string[] array = {"football", "soc", "12", "124"};
 // Console.WriteLine(array);
 void ReadArray()
 {   
@@ -15,4 +15,19 @@ void ReadArray()
     }
 Console.WriteLine();
 }
+string[] newArray = new string[array.Length];
+void PrintArray()
+{   int count = 0;
+    for (int i = 0; i < 4; i++)
+    {
+    if (array[i].Length <= 3)
+    {  
+        newArray[count] = array[i];
+        count++;
+        Console.Write($"{array[i]} ");
+    } 
+    }
+Console.WriteLine();
+}
 ReadArray();
+PrintArray();
