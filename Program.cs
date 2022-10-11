674462711da365f55563ed63331f0d7f343995f3
Daniil7 <den.urishov@yandex.ru>
2022-10-11 15:47:39 +0400

    metod 1

--- Program.cs
+++ Program.cs
@@ -2,4 +2,17 @@
 // которая из имеющегося массива строк 
 // формирует массив из строк длина которых 
 // меньше либо равна 3 символа.
-Console.WriteLine("Hello, World!");
+string[] array = {"football", "soccer", "123", "12"};
+// Console.WriteLine(array);
+void ReadArray()
+{   
+    for (int i = 0; i < 4; i++)
+    {
+    // if (array[i] != null )
+    // { 
+        Console.Write($"{array[i]} ");
+    // }
+    }
+Console.WriteLine();
+}
+ReadArray();
