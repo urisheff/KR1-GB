82895a53ff7c6c59084dc31627f91c4041778f93
Daniil7 <den.urishov@yandex.ru>
2022-10-12 15:10:45 +0400

    Gotovaya

--- Program.cs
+++ Program.cs
@@ -2,31 +2,30 @@
 // которая из имеющегося массива строк 
 // формирует массив из строк длина которых 
 // меньше либо равна 3 символа.
-string[] array = {"football", "soc", "12", "124"};
-// Console.WriteLine(array);
+string[] array = {"football", "yep", "1256", "125"};
+string[] newArray = new string[array.Length];
 void ReadArray()
 {   
     for (int i = 0; i < 4; i++)
     {
-    // if (array[i] != null )
-    // { 
+
         Console.Write($"{array[i]} ");
-    // }
     }
 Console.WriteLine();
 }
-string[] newArray = new string[array.Length];
 void PrintArray()
 {   int count = 0;
     for (int i = 0; i < 4; i++)
     {
-    if (array[i].Length <= 3)
-    {  
-        newArray[count] = array[i];
-        count++;
-        Console.Write($"{array[i]} ");
-    } 
+        if (array[i].Length <= 3)
+        {  
+            newArray[count] = array[i];
+            count++;
+            Console.Write($"{array[i]} ");
+        } 
+        
     }
+    
 Console.WriteLine();
 }
 ReadArray();
